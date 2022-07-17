8c887afa98e6afaa00fbb1ebdd4f02ed2347d1af
Katie Walters <katie_walters@live.co.uk>
2022-07-17 16:26:50 +0100

    list doesnt hide when review component shows. working otherwise

--- react-search/src/Components/Filter/Filter.js
+++ react-search/src/Components/Filter/Filter.js
@@ -1,15 +1,15 @@
 import { React } from 'react';
 import Title from '../Title/Title'
 
-function Filter({reviews }) {
-
+function Filter({reviews}) {
 console.log(reviews)
 
 
 
     if (reviews?.length > 0) {
         let filter = reviews.map((review, i) => 
-          <Title key={review.id} review={review} />
+          <Title key={review.id} review={review}/>
+          
           );
         console.log(filter)
         return (
--- react-search/src/Components/Title/Title.js
+++ react-search/src/Components/Title/Title.js
@@ -1,30 +1,26 @@
-import { React, useState} from 'react'
-import Review from '../Review/Review';
+import { React, useState } from 'react'
+import Review from '../Review/Review'
+
 
 function Title({review}) {
     const [reviewShown, setReviewShown] = useState(false);
-    const [itemVisible, setItemVisible] = useState(true);
-    
+    const [listVisible, setListVisible] = useState(true);
+
     console.log(review);
+
     const handleClick = event => {
-        // toggle shown state
-        setReviewShown(true);
-        setItemVisible(false);
-      };
-    
+        setReviewShown(true)
+        setListVisible(current => !current);
+    }
+
         return(
             <>
-            <div>
-
-                    <div >
-                            <p className="list-container2" style={{visibility: itemVisible ? 'visible' : 'hidden'}} onClick={handleClick}>{review.author}: {review.title}</p>
-
-                    </div>
-                    <div>
-                        {reviewShown && <Review review={review} />}
-                    </div>
-                   
+            <div className="list-container" style={{visibility: listVisible ? 'visible' : 'hidden' }}>
+                <p className="list-container2" onClick={handleClick}>{review.author}: {review.title}</p>
             </div>
+                <div>
+                    {reviewShown && <Review review={review}  />}
+                </div>
             </>
         )
 }
