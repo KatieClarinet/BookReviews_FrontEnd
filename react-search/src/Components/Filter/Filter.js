8f74e3abd565f81666c2f287b974d573123abc32
Katie Walters <katie_walters@live.co.uk>
2022-07-19 13:27:22 +0100

    working

--- react-search/src/Components/Filter/Filter.js
+++ react-search/src/Components/Filter/Filter.js
@@ -1,25 +1,19 @@
 import { React } from 'react';
-import Title from '../Title/Title'
+import Review from '../Review/Review';
 
 function Filter({reviews}) {
 console.log(reviews)
 
-
-
     if (reviews?.length > 0) {
         let filter = reviews.map((review, i) => 
-          <Title key={review.id} review={review}/>
-          
+          <Review key={review.id} review={review}/>
           );
         console.log(filter)
         return (
-          
            <div className="filter">{filter}</div>
-          
         )
       }
-       
-        }
+ }
 
- 
+
 export default Filter;
